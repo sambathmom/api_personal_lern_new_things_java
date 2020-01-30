530c23c2a428442e4965003ace0368bdd9a5c9bd
Sambath Mom <sambath.mom@truemoney.com.kh>
2020-01-30 08:43:43 +0700

    Config project with Postgresql db

---
+++ src/main/java/api/personallearn/newthings/NewThingsApplication.java
@@ -0,0 +1,13 @@
+package api.personallearn.newthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewThingsApplication.class, args);
+    }
+
+}
